0612ec3a946003258c9932677c107c0c03da6761
JHashimoto0518 <junichi_hashimoto@hotmail.co.jp>
2022-04-09 14:11:08 +0900

    Add: implement ec2list

--- JHashimoto.AwsViewer/JHashimoto.AwsViewer.Domain/Domain/Models/EC2/EC2InstanceList.cs
+++ JHashimoto.AwsViewer/JHashimoto.AwsViewer.Domain/Domain/Models/EC2/EC2InstanceList.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace JHashimoto.AwsViewer.CreateDefinitionsApplication.Domain.Models.EC2 {
-    public interface IEC2Repository {
-        IEnumerable<EC2Instance> GetAll();
+    public class EC2InstanceList {
+        public IEnumerable<EC2Instance>? Values { get; set; }
     }
 }
